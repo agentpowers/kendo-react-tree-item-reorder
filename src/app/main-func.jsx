8b837c353abfdcd4c5f77a44d2ebb217b6eb9d12
Roney George <georgeroney@hotmail.com>
2019-10-05 02:14:09 -0400

    WIP - got diff

--- src/app/main-func.jsx
+++ src/app/main-func.jsx
@@ -23,29 +23,32 @@ function iconClassName({ text, items }) {
 const SEPARATOR = '_';
 const tree = [{
     id: 100,
+    _id: 'f100',
     text: 'Furniture', 
     expanded: true,
     isFolder: true,
     items: [
-      { id: 1, text: 'Tables & Chairs.jpg' },
-      { id: 2, text: 'Sofas.pdf' },
-      { id: 3, text: 'Occasional Furniture.gif' }]
+      { id: 1, _id:'1', text: 'Tables & Chairs.jpg' },
+      { id: 2, _id:'2', text: 'Sofas.pdf' },
+      { id: 3, _id:'3', text: 'Occasional Furniture.gif' }]
   }, {
     id: 101,
+    _id:'f101',
     text: 'Decor',
     expanded: true,
     isFolder: true,
     items: [
-      { id: 4, text: 'Bed Linen.pdf' },
-      { id: 5, text: 'Curtains & Blinds.jpg' },
+      { id: 4, _id:'4', text: 'Bed Linen.pdf' },
+      { id: 5, _id:'5', text: 'Curtains & Blinds.jpg' },
       { 
         id: 102,
+        _id:'f102',
         text: 'Carpets',
         expanded: true,
         isFolder: true,
         items:[
-          { id: 6, text: "High Pile.html" },
-          { id: 7, text: "Low Pile.png" }
+          { id: 6, _id:'6', text: "High Pile.html" },
+          { id: 7, _id:'7', text: "Low Pile.png" }
         ]
       }
     ]
@@ -130,20 +133,199 @@ const getClueClassName = (event, tree) => {
 const ItemRenderer = (props) => {
     return (
         <>
-            <span className={iconClassName(props.item)} key='0'></span> {props.item.text}
+            <span className={iconClassName(props.item)} key='0'></span> {props.item.id}-{props.item.text}
         </>
     )
 }
 
-const getDiff = (a, b) => {
-    const changes = [];
-    if (a === b) {
-        return changes;
-    } else {
-        if (Array.isArray(a)) {
-            return null;
+const flatten = (items, parentId) => {
+    if(Array.isArray(items)) {
+        let result = {};
+        for (let index = 0; index < items.length; index++) {
+            const item = items[index];
+            result[item._id] = { item, index, parentId };
+            if (item.isFolder) {
+                result = {
+                    ...result,
+                    ...(flatten(item.items, item.id))
+                }
+            }
+        }
+        return result;
+    }
+    return {};
+};
+
+const rootFlatten = (item, index) => {
+    let result = {};
+    result[item._id] = { item, index, parentId: null };
+    if (item.isFolder) {
+        result = {
+            ...result,
+            ...(flatten(item.items, item.id))
+        };
+    }
+
+    return result;
+};
+
+const getDiff2 = (original, updated) => {
+    let changed = [];
+    let added = [];
+    let deleted = [];
+    let originalFlatten = {};
+    let updatedFlatten = {};
+    for (let index = 0; index < Math.max(original.length, updated.length); index++) {
+        if (original.length > index && updated.length > index) {
+            if (original[index] === updated[index]){
+                continue;
+            } else {
+                originalFlatten = {
+                    ...originalFlatten,
+                    ...(rootFlatten(original[index], index))
+                };
+                updatedFlatten = {
+                    ...updatedFlatten,
+                    ...(rootFlatten(updated[index], index))
+                };
+            }
+            continue;
+        }
+        if (original.length > index) {
+            originalFlatten = {
+                ...originalFlatten,
+                ...(rootFlatten(original[index], index))
+            };
+        }
+        if (updated.length > index) {
+            updatedFlatten = {
+                ...updatedFlatten,
+                ...(rootFlatten(updated[index], index))
+            };
         }
     }
+    Object.keys(updatedFlatten).forEach(key => {
+        const g = updatedFlatten[key];
+        var original = originalFlatten[key];
+        if(original) {
+            if (g.index !== original.index) {
+                changed.push({
+                    type: g.item.isFolder ? "folder-display-order-change" : "file-display-order-change",
+                    id: g.item.id,
+                    value: g.index
+                });
+            }
+            if (g.parentId !== original.parentId) {
+                changed.push({
+                    type: g.item.isFolder ? "folder-parent-change" : "file-parent-change",
+                    id: g.item.id,
+                    value: g.parentId
+                });
+            }
+        } else {
+            added.push({
+                type: g.item.isFolder ? "folder-added" : "file-added",
+                id: g.item.id,
+                value: g.parentId
+            });
+        }
+    });
+
+    // check for deleted
+    deleted = Object.keys(updatedFlatten).filter(f => !updatedFlatten[f]).map(f => updatedFlatten[f]).map(upd => {
+        return {
+            type: upd.isFolder ? "folder-deleted" : "file-deleted",
+            id: upd.item.id,
+            value: null
+        };
+    });
+
+    return { changed, added, deleted };
+}
+
+const getDiff = (original, updated, parentId) => {
+    let changes = {};
+    let added = {};
+    let deleted = {};
+    if (updated == null && Array.isArray(original)) {
+        for (let index = 0; index < original.length; index++) {
+            const orginalItem = original[index];
+            deleted[orginalItem.id] = {
+                type: orginalItem.isFolder ? "folder-deleted" : "file-deleted",
+                id: orginalItem.id,
+                value: null
+            };
+
+            if (orginalItem.isFolder) {
+                const { changes : c, added : a, deleted : d } = getDiff(orginalItem.items, null, orginalItem.id);
+                changes = {
+                    ...changes,
+                    ...c
+                };
+                added = {
+                    ...added,
+                    ...a
+                };
+                deleted = {
+                    ...deleted,
+                    ...d
+                };
+            }
+        }
+    }
+    if (Array.isArray(updated)) {
+        const originalIdMap = original ? original.reduce((acc, curr) => {
+            acc[curr._id] = true;
+            return acc;
+        }, {}) : {};
+        for (let index = 0; index < updated.length; index++) {
+            const updItm = updated[index];
+            const hasOriginalId = originalIdMap[updItm._id];
+            const hasOriginalAtSameIndex = original && (original.length > index) && (original[index].id === updated[index].id);
+
+            if (!hasOriginalAtSameIndex) {
+                // add display order change
+                changes[updItm._id] = {
+                    type: updItm.isFolder ? "folder-display-order-change" : "file-display-order-change",
+                    id: updItm.id,
+                    value: index
+                };
+            }
+
+            // if new item then add it
+            if (hasOriginalId){
+                delete originalIdMap[updItm._id]
+            } else {
+                added[updItm.id] = {
+                    type: updItm.isFolder ? "folder-added" : "file-added",
+                    id: updItm.id,
+                    value: parentId
+                };
+            }
+            if (updItm.isFolder) {
+                const { changes : c, added : a, deleted : d } = getDiff(hasOriginalAtSameIndex ? original[index].items : null, updItm.items, updItm.id);
+                changes = {
+                    ...changes,
+                    ...c
+                };
+                added = {
+                    ...added,
+                    ...a
+                };
+                deleted = {
+                    ...deleted,
+                    ...d
+                };
+            }
+        }
+        // if there are leftover in originalIdMap then they are deleted
+        deleted = Object.values(originalIdMap).map(item => ({
+            type: item.isFolder ? "folder-deleted" : "file-deleted",
+            id: item.id,
+            value: null
+        }));
+    }
+    return { changes, added, deleted };
 };
 
 const App = ({ tree }) => {
@@ -163,7 +345,6 @@ const App = ({ tree }) => {
 
         const { canDrop, eventAnalyzer, targetItem, itemHierarchicalIndex, targetHierarchicalIndex } = getEventMeta(event, treeState.tree);
         if (canDrop) {
-            debugger;
             const originalDropOp = eventAnalyzer.getDropOperation();
             // we don't need child operations on files- forcing child to an "after"
             const dropOp = (originalDropOp === "child"  && targetItem.isFolder) ? originalDropOp : "after";
@@ -173,6 +354,8 @@ const App = ({ tree }) => {
                 dropOp,
                 targetHierarchicalIndex,
             );
+            const diff = getDiff2(treeState.tree, updatedTree);
+            console.log(diff);
             // update state
             setTreeState({ tree: updatedTree });
         }
